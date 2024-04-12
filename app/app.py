67ce6f80b62d485624dd071600ad224478180d70
Nguyen Quoc Truong <quoctruong3105@gmail.com>
2024-04-12 21:57:54 +0700

    Ugrade for proccessing more page

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request
 import requests
 import json
-import config
+from config import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '63da23qw-b54c-2g6a-90ae-2f54d5jf6c82'
 
 
-def callSendAPI(senderPsid, response):
-    PAGE_ACCESS_TOKEN = config.PAGE_ACCESS_TOKEN
+def callSendAPI(senderPsid, recipientPsid, response):
+    PAGE_ACCESS_TOKEN = PAGE_ACCESS_TOKEN_LIST.get(recipientPsid)
     payload = {
         'recipient': {'id': senderPsid},
         'message': response,
@@ -16,17 +16,17 @@ def callSendAPI(senderPsid, response):
     }
     headers = {'content-type': 'application/json'}
     url = 'https://graph.facebook.com/v10.0/me/messages?access_token={}'.format(PAGE_ACCESS_TOKEN)
-    r = requests.post(url, json=payload, headers=headers)
-    print(r.text)
-    
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
 
-def handleMessage(senderPsid, receiveMessage):
+
+def handleMessage(senderPsid, recipientPsid, receiveMessage):
     if 'text' in receiveMessage:
         response = {"text": 'You just sent me: {}'.format(receiveMessage['text'])}
-        callSendAPI(senderPsid, response)
+        callSendAPI(senderPsid, recipientPsid, response)
     else:
         response = {"text": 'This chatbot only accepts text messages'}
-        callSendAPI(senderPsid, response )
+        callSendAPI(senderPsid, recipientPsid, response )
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,24 +37,22 @@ def home():
 @app.route('/webhook', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        VERIFY_TOKEN = '2PK5sbFAaoxK7FqI91FuT2gZ8vn_2Q2dLJAxkbksQrU8URr3X'
-
         if 'hub.mode' in request.args:
             mode = request.args.get('hub.mode')
-            print(mode)
+            # print(mode)
         if 'hub.verify_token' in request.args:
             token = request.args.get('hub.verify_token')
-            print(token)
+            # print(token)
         if 'hub.challenge' in request.args:
             challenge = request.args.get('hub.challenge')
-            print(challenge)
+            # print(challenge)
             
         if 'hub.mode' in request.args and 'hub.verify_token' in request.args:
             mode = request.args.get('hub.mode')
             token = request.args.get('hub.verify_token')
             
-            if mode == 'subscribe' and token == VERIFY_TOKEN:
-                print('WEBHOOK_VERIFIED')
+            if mode == 'subscribe' and token == SERVER_VERIFY_TOKEN:
+                #print('WEBHOOK_VERIFIED')
                 challenge = request.args.get('hub.challenge')
                 return challenge, 200
             else:
@@ -63,24 +61,22 @@ def index():
         return 'SOMETHING', 200
 
     if request.method == 'POST':
-        VERIFY_TOKEN = '2PK5sbFAaoxK7FqI91FuT2gZ8vn_2Q2dLJAxkbksQrU8URr3X'
-
         if 'hub.mode' in request.args:
             mode = request.args.get('hub.mode')
-            print(mode)
+            # print(mode)
         if 'hub.verify_token' in request.args:
             token = request.args.get('hub.verify_token')
-            print(token)
+            # print(token)
         if 'hub.challenge' in request.args:
             challenge = request.args.get('hub.challenge')
-            print(challenge)
+            # print(challenge)
             
         if 'hub.mode' in request.args and 'hub.verify_token' in request.args:
             mode = request.args.get('hub.mode')
             token = request.args.get('hub.verify_token')
             
-            if mode == 'subscribe' and token == VERIFY_TOKEN:
-                print('WEBHOOK_VERIFIED')
+            if mode == 'subscribe' and token == SERVER_VERIFY_TOKEN:
+                # print('WEBHOOK_VERIFIED')
                 challenge = request.args.get('hub.challenge')
                 return challenge, 200
             else:
@@ -93,13 +89,14 @@ def index():
         if 'object' in body and body['object'] == 'page':
             entries = body['entry']
             for entry in entries:
+                print(entry)
                 webhookEvent = entry['messaging'][0]
                 print(webhookEvent)
                 senderPsid = webhookEvent['sender']['id']
+                recipientPsid = webhookEvent['recipient']['id']
                 print('Sender PSID: {}'.format(senderPsid))
-                
                 if 'message' in webhookEvent: 
-                    handleMessage(senderPsid, webhookEvent['message'])
+                    handleMessage(senderPsid, recipientPsid, webhookEvent['message'])
                     
                 return 'EVENT_RECEIVED', 200
         else:
